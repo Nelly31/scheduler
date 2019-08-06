7b2d777fe5f0647ec8c28eea457168f8f6182bb0
Nelly31 <nelly_main@hotmail.com>
2019-08-06 23:06:16 +0000

    adding days from server db

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,28 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 
 import DayList from "components/DayList.js";
 import Appointment from "components/Appointment"
 import { tsPropertySignature } from "@babel/types";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -63,6 +46,16 @@ const appointments = [
 export default function Application(props) {
 
   const [count, setCount] = useState("Monday")
+  const [days, setDays] = useState([]) 
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/api/days')
+      .then((response) => {
+        console.log("XXXXXXXXXX",response.data)
+        setDays(response.data);
+      })
+      .catch(error => console.log(error))
+  }, [])
 
   const appointmentList = appointments.map(appointment => <Appointment key={appointment.id} id={appointment.id} time={appointment.time} interview={appointment.interview} />)
   
--- src/components/Appointment/Header.js
+++ src/components/Appointment/Header.js
@@ -4,9 +4,9 @@ import "components/Appointment/style.scss"
 export default function Application(props) {
   
   return (
-    <header class="appointment__time">
-      <h4 class="text--semi-bold">{props.time}</h4>
-      <hr class="appointment__separator" />
+    <header className="appointment__time">
+      <h4 className="text--semi-bold">{props.time}</h4>
+      <hr className="appointment__separator" />
     </header>
   )
 }
