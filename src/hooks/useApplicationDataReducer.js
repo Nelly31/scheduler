6fcc5c63472ab6461dfc5918f0c7e0eeca4c78aa
Nelly31 <nelly_main@hotmail.com>
2019-10-16 22:04:36 +0000

    added circlici file

---
+++ src/hooks/useApplicationDataReducer.js
@@ -0,0 +1,148 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export function useApplicationData(initial) {
+  useEffect(() => {
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
+    ]).then((all) => {
+      const [daysResult, apptResult, interviewersResult] = all;
+      setState(prev => ({ ...prev, days: daysResult.data, appointments: apptResult.data, interviewers: interviewersResult.data }))
+    })
+      .catch(error => console.log(error))
+  }, []);
+
+  //State object
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+
+  //setDay
+  const setDay = "setDay";
+  const bookInterview = "bookInterview";
+
+  //bookInterview
+  //add a new interview obj for the set appointment id
+  const bookInterview = function (id, interview) {
+
+    //function to update the relevant day obj in the day array. 
+    function updateObjectInArray(days, id) {
+      
+      return days.map(day => {
+        if (day.appointments.includes(id)) {
+          day.spots = day.spots - 1
+        }
+      
+        return {  
+          ...days,
+          ...day,
+        }
+      })
+      
+    }
+
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview },
+    };
+
+    const newDays = updateObjectInArray(state.days, id)
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    return axios.put(`http://localhost:3001/api/appointments/${id}`, {
+      interview,
+    })
+      .then(() => {
+        setState({
+          ...state,
+          appointments,
+          days: newDays
+        })
+      })
+
+  };
+
+//edit Interview 
+
+const editInterview = function (id, interview) {
+
+  const appointment = {
+    ...state.appointments[id],
+    interview: { ...interview },
+  };
+
+  const appointments = {
+    ...state.appointments,
+    [id]: appointment
+  };
+
+  return axios.put(`http://localhost:3001/api/appointments/${id}`, {
+    interview,
+  })
+    .then(() => {
+      setState({
+        ...state,
+        appointments
+      })
+    })
+
+};
+
+  //Delete Interview
+  const deleteInterview = function (id, interview) {
+    function updateObjectInArray(days, id) {
+      
+      return days.map(day => {
+        if (day.appointments.includes(id)) {
+          day.spots = day.spots + 1
+        }
+      
+        return {  
+          ...days,
+          ...day,
+        }
+      })
+      
+    }
+
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+
+    const newDays = updateObjectInArray(state.days, id)
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    return axios.delete(`http://localhost:3001/api/appointments/${id}`, {
+      interview,
+    })
+      .then(() => {
+        setState({
+          ...state,
+          appointments,
+          days: newDays
+        })
+      })
+  };
+
+  return {
+    state,
+    setDay,
+    bookInterview,
+    deleteInterview,
+    editInterview
+  }
+}
